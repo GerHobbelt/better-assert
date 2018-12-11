ee3bb3013c724b3c0fd4b6e244f2a0caf1ae89b2
Ger Hobbelt <ger@hobbelt.com>
2018-12-11 21:48:57 +0100

    augmented tests to validate fix for https://github.com/tj/better-assert/issues/12: when the test/testtest.js file is pulled through `babel` the tests coded in there would fail due the code having been prettyfied by babel. The tests have been adjusted.

--- test/testtest.js
+++ test/testtest.js
@@ -16,8 +16,12 @@ try {
 
 	// tests which serve as simple better-assert library unit tests:
 	console.log("\n\n==================================\n\n## Unit Tests for the library ##\n\n");
-	
-	assert(ex.message.indexOf("(a==b,`${a} should be ${b}`) 1 should be 2") >= 0);
+
+	assert(
+		// this check will match when run directly in node (v8, v10, ...)	
+		ex.message.indexOf("(a==b,`${a} should be ${b}`) 1 should be 2") >= 0 ||
+		// while this check will match when run via a babel transpile (which prettyfies the code)	
+		ex.message.indexOf("(a == b, `${a} should be ${b}`) 1 should be 2") >= 0);
 	assert(ex.name.indexOf("AssertionError") === 0);
 
 	console.log("PASS OK");
