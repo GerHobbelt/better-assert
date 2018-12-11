d57880662da53093048fde545e55365118910676
Ger Hobbelt <ger@hobbelt.com>
2018-12-11 20:39:20 +0100

    turned example.js into a demo + unit tests file which is now also used by `npm test` task.

--- example.js
+++ example.js
@@ -1,10 +1,24 @@
 
 var assert = require('./');
 
-test();
 
 function test() {
   var user = { name: 'tobi' };
   assert('tobi' == user.name);
   assert('number' == typeof user.age);
+}
+
+try {
+	test();
+} catch (ex) {
+	console.log("## Demo ##\n");
+	console.log(ex);
+
+	// tests which serve as simple better-assert library unit tests:
+	console.log("\n\n==================================\n\n## Unit Tests for the library ##\n\n");
+	
+	assert(ex.message.indexOf("'number' == typeof user.age") >= 0);
+	assert(ex.name.indexOf("AssertionError") === 0);
+
+	console.log("PASS OK");
 }
